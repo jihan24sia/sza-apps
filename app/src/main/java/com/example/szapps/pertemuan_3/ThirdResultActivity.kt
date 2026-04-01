af351e71210ca0ed99cb7857f5e0b50d67048307
jihan24sia <jihan24si@mahasiswa.pcr.ac.id>
2026-04-01 16:42:04 +0700

    Penerapan Intent

---
+++ app/src/main/java/com/example/szapps/pertemuan_3/ThirdResultActivity.kt
@@ -0,0 +1,26 @@
+package com.example.szapps.pertemuan_3
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.szapps.R
+
+class ThirdResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_third_result)
+
+        val nomor = intent.getStringExtra("nomor")
+
+        val txtNomor = findViewById<TextView>(R.id.txtNomor)
+        val btnKembali = findViewById<Button>(R.id.btnKembali)
+
+        txtNomor.text = nomor
+
+        btnKembali.setOnClickListener {
+            finish()
+        }
+    }
+}
